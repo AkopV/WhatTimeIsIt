e3dd89240c70e9ab76a8d87cca6f490c455e438b
VardanianAkop <AkopVV@gmail.com>
2016-10-06 17:57:05 +0300

    create message properties

--- src/test/com/vardanian/WhatTimeIsItTest.java
+++ src/test/com/vardanian/WhatTimeIsItTest.java
@@ -1,30 +1,51 @@
 package com.vardanian;
 
-import com.vardanian.WhatTimeIsIt;
-import org.junit.Assert;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static com.vardanian.WhatTimeIsIt.helloMessage;
+import java.time.LocalTime;
+
+import static com.vardanian.WhatTimeIsIt.showMessage;
 
 public class WhatTimeIsItTest {
 
     @Test
-    public void testMorning(){
-        Assert.assertEquals("Good morning, World!", helloMessage(8));
+    public void testMorningOne(){
+        Assert.assertEquals("Good morning, World!", showMessage(6));
     }
 
     @Test
-    public void testDay(){
-        Assert.assertEquals("Good day, World!", helloMessage(9));
+    public void testMorningTwo(){
+        Assert.assertEquals("Good morning, World!", showMessage(LocalTime.parse("06:00:01")));
     }
 
     @Test
-    public void testEvening(){
-        Assert.assertEquals("Good evening, World!", helloMessage(22));
+    public void testDayOne(){
+        Assert.assertEquals("Good day, World!", showMessage(9));
     }
 
     @Test
-    public void testNight(){
-        Assert.assertEquals("Good night, World!", helloMessage(5));
+    public void testDayTwo(){
+        Assert.assertEquals("Good day, World!", showMessage(LocalTime.parse("09:50:00")));
+    }
+
+    @Test
+    public void testEveningOne(){
+        Assert.assertEquals("Good evening, World!", showMessage(22));
+    }
+
+    @Test
+    public void testEveningTwo(){
+        Assert.assertEquals("Good evening, World!", showMessage(LocalTime.parse("22:00:00")));
+    }
+
+    @Test
+    public void testNightOne(){
+        Assert.assertEquals("Good night, World!", showMessage(5));
+    }
+
+    @Test
+    public void testNightTwo(){
+        Assert.assertEquals("Good night, World!", showMessage(LocalTime.parse("05:00:00")));
     }
 }
